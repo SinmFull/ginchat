b32e87fc5f8b4e7b472e7f1539c444c147fe7f1a
sinmfull <1747373447@qq.com>
2024-08-14 14:22:01 +0800

    添加好友优化

--- models/contact.go
+++ models/contact.go
@@ -34,20 +34,49 @@ func SearchFriend(userId uint) []UserBasic {
 	return users
 }
 
-func AddFriend(userId uint, targetId uint) int {
-	fmt.Println("user>>", userId, "   tarID>>>>>", targetId)
+func AddFriend(userId uint, targetId uint) (int, string) {
+	//fmt.Println("user>>", userId, "   tarID>>>>>", targetId)
 	user := UserBasic{}
 	if targetId != 0 {
 		user = FindUserByID(targetId)
 		if user.Salt != "" {
+
+			if userId == user.ID {
+				return -1, "不能添加自己为好友"
+			}
+			contact0 := Contact{}
+			utils.DB.Where("owner_id = ? and target_id = ? and type = 1", userId, targetId).Find(&contact0)
+			if contact0.ID != 0 {
+				return -1, "该用户已经是您的好友"
+			}
+
+			tx := utils.DB.Begin()
+			//事务一旦开始 ， 不论期间什么异常最终都会rollback
+			defer func() {
+				if r := recover(); r != nil {
+					tx.Rollback()
+				}
+			}()
 			contact := Contact{}
 			contact.OwnerId = userId
 			contact.TargetId = targetId
 			contact.Type = 1
-			utils.DB.Create(&contact)
-			return 0
+			if err := utils.DB.Create(&contact).Error; err != nil {
+				tx.Rollback()
+				return -1, "添加好友失败"
+			}
+			contact1 := Contact{}
+			contact1.OwnerId = targetId
+			contact1.TargetId = userId
+			contact1.Type = 1
+			if err := utils.DB.Create(&contact1).Error; err != nil {
+				tx.Rollback()
+				return -1, "添加好友失败"
+			}
+			tx.Commit()
+			return 0, "添加好友成功"
 		}
-		return -1
+		return -1, "该用户不存在"
 	}
-	return -1
+	return -1, "请输入好友ID"
 }
